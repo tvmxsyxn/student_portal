a9f47f7bf68395f2285e27d3d86eb21002034066
Студент <student@istu.int>
2026-03-06 10:29:39 +0800

    Initial commit: Add main function

---
+++ app.py
@@ -0,0 +1,9 @@
+"""Simple Student Portal Application"""
+
+def main():
+    print("Student Portal System")
+
+if __name__ == "__main__":
+    main()
+    # Инициализируем Git репозиторий в текущей папке
+git init
